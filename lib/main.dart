7b608ccad1624e581505422422ebc4ff7d96ffbd
Jun Lee <bozloo@gmail.com>
2022-04-13 19:48:53 -0500

    snapbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,211 +8,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Demo",
+      title: 'Snack bar demo',
+      home: MyPage(),
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text("Demo"),
-        backgroundColor: Colors.amber[700],
+        title: Text('Snack bar'),
         centerTitle: true,
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: (){
-              print('shopping_cart button is clicked');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){
-              print('search button is clicked');
-            },
-          ),
-        ],
+        backgroundColor: Colors.redAccent,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/monkey.jpeg'),
-                backgroundColor: Colors.red[200],
-              ),
-              otherAccountsPictures: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/google.png'),
-                  backgroundColor: Colors.yellow[200],
-                ),
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/google.png'),
-                  backgroundColor: Colors.yellow[200],
-                ),
-              ],
-              accountName: Text('bozlo'),
-              accountEmail: Text('bozlo@hotmail.com'),
-              onDetailsPressed: () {
-                print("Detailed is pressed");
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),
-                  bottomRight: Radius.circular(40.0)
-                )
-              ),
-            ),
-          ListTile(
-            leading: Icon(Icons.home,
-            color: Colors.grey[850],
-            ),
-            title: Text("Home"),
-            onTap: () {
-              print("home is clicked");
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-              leading: Icon(Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text("Setting"),
-              onTap: () {
-                print("Setting is clicked");
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ListTile(
-              leading: Icon(Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text("Q&A"),
-              onTap: () {
-                print("Q&A is clicked");
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/circle.gif'),
-                radius: 60.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30,
-            ),
-            Text("Name",
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            ),),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('BBANTO',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text("BBANTO POWER LEVEL",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('14',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),),
-              SizedBox(
-                height: 30.0,
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text("using lightsaber",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),),
-                ],
-              ),
-              Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("face hero tattoo",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),),
-              ],
-            ),
-              Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("fire flames",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/google.png'),
-                radius: 60.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: MySnapBar(),
     );
   }
 }
+
+class MySnapBar extends StatelessWidget {
+  const MySnapBar({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+       child: ElevatedButton(
+       child: Text('Show me',
+          style: TextStyle(
+          color: Colors.white,
+          ),
+        ),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Snapbar',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.orange
+              ),),
+            duration: Duration(milliseconds: 3000),
+            backgroundColor: Colors.teal,
+            action: SnackBarAction(
+              onPressed: () {
+                print('snapBar is clicked');
+              }, label: 'Haha',
+            ),
+          )
+          );
+        }),
+    );
+  }
+}
+
 
