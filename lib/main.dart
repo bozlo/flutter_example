8b2bc02c268895f796704b00b7d85c856d2434e4
Jun Lee <bozloo@gmail.com>
2022-04-14 22:11:32 -0500

    snackbar 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'ScreenA.dart';
-import 'ScreenB.dart';
-import 'ScreenC.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,16 +8,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigator demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context)=> const ScreenA(),
-        '/b': (context)=>const ScreenB(),
-        '/c': (context)=>const ScreenC(),
-      },
+      title: "Scaffold Messenger",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scaffold Messenger'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to the Screen Page'),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context)=>SecondPage(),
+            ),);
+          },
+        ),
       ),
     );
   }
 }
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scaffold Messenger'),
+      ),
+      body: Center(
+        child: Text('Hello world'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Like a new snack bar!'),
+            duration: Duration(seconds: 5),
+            action: SnackBarAction(
+              label: "OK",
+              onPressed: () {  },
+            ),
+
+          ));
+        },
+      ),
+
+    );
+  }
+}
+
