168cf276fb7c6ef31ca9edb4b2501bdc010583f5
Jun Lee <bozloo@gmail.com>
2022-04-14 22:29:55 -0500

    snackbar 3

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,23 @@ class FirstPage extends StatelessWidget {
           },
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Like a new snack bar!'),
+            duration: Duration(seconds: 5),
+            action: SnackBarAction(
+              label: "Undo",
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => ThirdPage(),
+                ));
+              },
+            ),
+          ));
+        },
+      ),
     );
   }
 }
@@ -47,27 +64,55 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Scaffold Messenger'),
+        title: Text('Second Page'),
       ),
       body: Center(
-        child: Text('Hello world'),
+        child: Text('Add to favorite!!!'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.thumb_up),
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text('Like a new snack bar!'),
-            duration: Duration(seconds: 5),
-            action: SnackBarAction(
-              label: "OK",
-              onPressed: () {  },
-            ),
-
-          ));
-        },
-      ),
-
     );
   }
 }
 
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ThridPage를 벗어나면 context가 의미가 없어져서 snackbar가 바로 없어진다.
+    return ScaffoldMessenger(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+        ),
+        body: Builder(
+          builder: (context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('want to undo Favorite?',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          duration: Duration(seconds: 5),
+                          action: SnackBarAction(
+                              label: 'Cancel', onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                          content: Text('It is undo'),));
+                      },
+                      child: Text('Cancel'))
+                ],
+              ),
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
